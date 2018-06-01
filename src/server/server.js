fc2c4bb5c3594ea68d47d41f65752c5e8eb6c65c
jakub krhovjak <jakub.krhojvak@aevi.cz>
2018-06-01 19:55:42 +0200

    Fixed dev

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const server = express()
+const express = require('express');
+const server = express();
+const path = require('path');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 server.get('/getData', (req, res) => {
     foo("dddd")(req, res)
@@ -12,13 +13,13 @@ const foo =  (data) => (req, res) =>
     res.send(data);
 
 
-if(process.env.NODE_ENV === 'procuction') {
-  const webpackMiddleware = require('webpack-dev-middleware')
-  const webpack = require('webpack')
-  const webpackConfig = require('../../webpack.dev')
+if(process.env.NODE_ENV !== 'procuction') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('../../webpack.dev');
   server.use(webpackMiddleware(webpack(webpackConfig)))
 } else {
-  server.use(express.static('public'))
+  server.use(express.static('public'));
   server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
   })
@@ -26,4 +27,4 @@ if(process.env.NODE_ENV === 'procuction') {
 
  
 server.listen(port, () => console.log(`Garage player server stared on port: ${port}`))
-
+;
